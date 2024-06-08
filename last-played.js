babaf6ece1adb918f685dbd786c8ff45e3078016
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-06-08 22:04:00 +0200

    Update last-played.js

--- last-played.js
+++ last-played.js
@@ -1,6 +1,6 @@
 // This plugin only works for myself at this moment.
 fetch('https://froodooo.pythonanywhere.com/last-played')
-  .then(res => res.text())
+  .then(res => res.json())
   .then((track) => {
     document.querySelector('#last-played').innerHTML=`My last played song is ${track.title} by ${track.artist} (checked using <a href='https://www.last.fm/user/Froodooo' target='_blank'>Last.fm</a>).`;
   });
