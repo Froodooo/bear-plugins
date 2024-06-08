291b130fffb845ef9eb8fe0bf047461daae87437
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-06-08 21:28:59 +0200

    Update last-played.js

--- last-played.js
+++ last-played.js
@@ -2,5 +2,5 @@
 fetch('https://froodooo.pythonanywhere.com/last-played')
   .then(res => res.text())
   .then((textResponse) => {
-    document.querySelector('#last-played').innerHTML=`My last played song is ${textResponse} (checked using <a href='https://www.last.fm/user/Froodooo target='_blank'>Last.fm</a>).`;
+    document.querySelector('#last-played').innerHTML=`My last played song is ${textResponse} (checked using <a href='https://www.last.fm/user/Froodooo' target='_blank'>Last.fm</a>).`;
   });
