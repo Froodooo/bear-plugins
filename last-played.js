920d0cc21e0a2b69a66d2a478d1142c31181535b
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-06-08 21:26:43 +0200

    Create last-played.js

---
+++ last-played.js
@@ -0,0 +1,6 @@
+// This plugin only works for myself at this moment.
+fetch('https://froodooo.pythonanywhere.com/last-played')
+  .then(res => res.text())
+  .then((textResponse) => {
+    document.querySelector('#last-played').innerHTML= `My last played song is ${textResponse} (checked using <a href='https://www.last.fm/user/Froodooo` target='_blank'>Last.fm</a>).";
+  });
