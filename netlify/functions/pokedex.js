7a72c2200b9ea95f423a9ea6f47f7b5ce1b1b6f2
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-06-09 14:29:16 +0200

    Set up a Netlify site with 2 serverless functions.

---
+++ netlify/functions/pokedex.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch'
+
+export const handler = async () => {
+    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+  
+    const response = await fetch(POKE_API)
+    const data = await response.json()
+  
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data
+      })
+    }
+  }
