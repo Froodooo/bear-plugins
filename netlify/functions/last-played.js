cda2faee602643df1ab9e41a6a52dd48cb49a301
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-06-09 14:50:32 +0200

    Update last played plugin

--- netlify/functions/last-played.js
+++ netlify/functions/last-played.js
@@ -1,12 +1,24 @@
 import fetch from 'node-fetch'
 
 export const handler = async () => {
-    data = {message: "Hello World!"}
+    const LASTFM_API = 'https://ws.audioscrobbler.com'
+    const API_KEY = process.env.LASTFM_API_KEY
+    const USER = process.env.LASTFM_USER
+    const ENDPOINT = `${LASTFM_API}/2.0/?method=user.getrecenttracks&user=${USER}&api_key=${API_KEY}&format=json&limit=1`
+  
+    const response = await fetch(ENDPOINT)
+    const data = await response.json()
+
+    const track = data.recenttracks.track[0]
+
+    const result = {
+        artist: track.artist['#text'],
+        name: track.name,
+        url: track.url
+    }
   
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        data
-      })
+      body: JSON.stringify(result)
     }
   }
