a9620cbe1da65d72ec7b32774d0285f2bf74586c
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-06-12 15:25:31 +0200

    Use target blank for post-scriptum

--- bear/post-scriptum.js
+++ bear/post-scriptum.js
@@ -8,9 +8,9 @@ if (document.body.classList.contains("post")) {
         innerHTML: `If you have any questions or comments, please contact me via ${document.currentScript.getAttribute("data").split(";").map((item) => {
           const [key, value] = item.split("::");
           if (key === "email") {
-            return `<a href="mailto:${value}">${key}</a>`;
+            return `<a href="mailto:${value}" target="_blank">${key}</a>`;
           } else {
-            return `<a href="${value}">${key}</a>`;
+            return `<a href="${value}" target="_blank">${key}</a>`;
           }
         }).join(" or ")
           }.`
