93a7add545fa08e2343d4fe73edf054fcacc4dc7
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-06-12 15:04:04 +0200

    Reformat code

--- bear/post-scriptum.js
+++ bear/post-scriptum.js
@@ -6,7 +6,6 @@ if (document.body.classList.contains("post")) {
         document.createElement("p"), {
         className: "post-scriptum",
         innerHTML: "If you have any questions or comments, please contact me via <a href='mailto:blog@yordi.me' target='_blank'>email</a> or <a href='https://social.lol/@yordi' target='_blank'>Mastodon</a>."
-      }
-      )
+      })
     );
 }
