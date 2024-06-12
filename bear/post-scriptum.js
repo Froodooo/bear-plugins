500de9bd25c70e014bc58ac82294d3b85431e1f2
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-06-12 15:21:38 +0200

    Update post scriptum to be more variable

--- bear/post-scriptum.js
+++ bear/post-scriptum.js
@@ -5,7 +5,15 @@ if (document.body.classList.contains("post")) {
       Object.assign(
         document.createElement("p"), {
         className: "post-scriptum",
-        innerHTML: "If you have any questions or comments, please contact me via <a href='mailto:blog@yordi.me' target='_blank'>email</a> or <a href='https://social.lol/@yordi' target='_blank'>Mastodon</a>."
+        innerHTML: `If you have any questions or comments, please contact me via ${document.currentScript.getAttribute("data").split(";").map((item) => {
+          const [key, value] = item.split(":");
+          if (key === "email") {
+            return `<a href="mailto:${value}">${key}</a>`;
+          } else {
+            return `<a href="${value}">${key}</a>`;
+          }
+        }).join(" or ")
+          }.`
       })
     );
 }
