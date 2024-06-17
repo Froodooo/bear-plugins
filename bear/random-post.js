a55f37db6cf693ffd793e9c6c00cc77d85808a14
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-06-17 21:11:05 +0200

    Add new random blog post plugin

---
+++ bear/random-post.js
@@ -0,0 +1,9 @@
+const allBlogPosts = new DOMParser()
+  .parseFromString(
+    (await(await fetch(document.currentScript.getAttribute("data-blog"))).text()), 'text/html'
+  ).querySelector(".blog-posts").children;
+
+const randomBlogPost = allBlogPosts[Math.floor(Math.random() * allBlogPosts.length)];
+
+document.querySelector('#random-post').innerHTML = `Random post: <a href='${randomBlogPost.children[1].href}'>${randomBlogPost.children[1].text}</a>`;
+document.querySelector('#random-post').classList.remove("blurred");
