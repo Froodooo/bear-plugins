e7534603f7f154e884ac710586239fded80a4566
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-06-18 08:42:44 +0200

    Random post prefix is empty by default

--- bear/random-post.js
+++ bear/random-post.js
@@ -3,6 +3,6 @@ if (document.querySelector("body").classList.contains("home")) {
     .querySelector(".blog-posts")
     .children[Math.floor(Math.random() * document.querySelector(".blog-posts").children.length)];
   document.querySelector('#random-post-all').remove();
-  document.querySelector('#random-post').innerHTML = `${document.currentScript.getAttribute("data-prefix") ?? "Random post: "}<a href='${randomBlogPost.children[1].href}'>${document.currentScript.getAttribute("data-text") ?? randomBlogPost.children[1].text}</a>`;
+  document.querySelector('#random-post').innerHTML = `${document.currentScript.getAttribute("data-prefix") ?? ""}<a href='${randomBlogPost.children[1].href}'>${document.currentScript.getAttribute("data-text") ?? randomBlogPost.children[1].text}</a>`;
   document.querySelector('#random-post').classList.remove("blurred");
 }
