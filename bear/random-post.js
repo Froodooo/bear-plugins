667270a0d007589a76fc307c76c7a7835ea50bab
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-08-18 14:58:26 +0200

    Use correct element to select random post

--- bear/random-post.js
+++ bear/random-post.js
@@ -1,6 +1,6 @@
 if (document.querySelector("body").classList.contains("home")) {
   const randomBlogPost = document
-    .querySelector(".blog-posts")
+    .querySelector("#random-post-all > .blog-posts")
     .children[Math.floor(Math.random() * document.querySelector(".blog-posts").children.length)];
   document.querySelector('#random-post-all').remove();
   document.querySelector('#random-post').innerHTML = `${document.currentScript.getAttribute("data-prefix") ?? ""}<a href='${randomBlogPost.children[1].href}'>${document.currentScript.getAttribute("data-text") ?? randomBlogPost.children[1].text}</a>`;
