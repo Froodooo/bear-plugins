616e488f5435bf53b1193495967a678e10e594c0
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-08-18 15:01:33 +0200

    Add id element to queryselector

--- bear/random-post.js
+++ bear/random-post.js
@@ -1,7 +1,7 @@
 if (document.querySelector("body").classList.contains("home")) {
   const randomBlogPost = document
     .querySelector("#random-post-all > .blog-posts")
-    .children[Math.floor(Math.random() * document.querySelector(".blog-posts").children.length)];
+    .children[Math.floor(Math.random() * document.querySelector("#random-post-all > .blog-posts").children.length)];
   document.querySelector('#random-post-all').remove();
   document.querySelector('#random-post').innerHTML = `${document.currentScript.getAttribute("data-prefix") ?? ""}<a href='${randomBlogPost.children[1].href}'>${document.currentScript.getAttribute("data-text") ?? randomBlogPost.children[1].text}</a>`;
   document.querySelector('#random-post').classList.remove("blurred");
