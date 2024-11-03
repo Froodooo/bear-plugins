e3d0f4e05963f9b3809bd53c9ab94f9b0caddbca
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-11-03 13:54:35 +0100

    Update reading.js to match Netlify function

--- bear/reading.js
+++ bear/reading.js
@@ -1,6 +1,7 @@
 if (document.querySelector("body").classList.contains("home")) {
   fetch('https://bear-plugins.netlify.app/.netlify/functions/reading')
     .then(res => res.json())
+    .then(books => books[Math.floor(Math.random() * books.length)])
     .then((book) => {
       document.querySelector('#reading')
         .innerHTML = `<a href='https://hardcover.app/books/${book.slug}' target='_blank'>${book.title} (${book.author})</a>`;
