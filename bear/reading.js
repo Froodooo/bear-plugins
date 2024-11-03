78079277bd6f1660a71629cbb6678d92ef54ad53
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-11-03 15:42:28 +0100

    Update last played and reading

--- bear/reading.js
+++ bear/reading.js
@@ -4,7 +4,7 @@ if (document.querySelector("body").classList.contains("home")) {
     .then(books => books[Math.floor(Math.random() * books.length)])
     .then((book) => {
       document.querySelector('#reading')
-        .innerHTML = `<a href='https://hardcover.app/books/${book.slug}' target='_blank'>${book.title} (${book.author})</a>`;
+        .innerHTML = `<a href='https://hardcover.app/books/${book.slug}' target='_blank'><img src="${book.url}" alt="${book.title} (${book.author})" height="150"><p>${book.title} (${book.author})</p></a>`;
       document.querySelector('#reading')
         .classList.remove("blurred");
     });
--- netlify/functions/last-played.js
+++ netlify/functions/last-played.js
@@ -1,25 +1,28 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
 export const handler = async () => {
-    const LASTFM_API = 'https://ws.audioscrobbler.com'
-    const API_KEY = process.env.LASTFM_API_KEY
-    const USER = process.env.LASTFM_USER
-    const ENDPOINT = `${LASTFM_API}/2.0/?method=user.getrecenttracks&user=${USER}&api_key=${API_KEY}&format=json&limit=1`
+    const LASTFM_API = 'https://ws.audioscrobbler.com';
+    const API_KEY = process.env.LASTFM_API_KEY;
+    const USER = process.env.LASTFM_USER;
+    const ENDPOINT = `${LASTFM_API}/2.0/?method=user.getrecenttracks&user=${USER}&api_key=${API_KEY}&format=json&limit=1`;
 
-    const response = await fetch(ENDPOINT)
-    const data = await response.json()
+    const response = await fetch(ENDPOINT);
+    const data = await response.json();
 
-    const track = data.recenttracks.track[0]
+    const track = data.recenttracks.track[0];
+
+    console.log(track);
 
     const result = {
         artist: track.artist['#text'],
         title: track.name,
-        url: track.url
-    }
+        url: track.url,
+        image: track.image[3]['#text']
+    };
 
     return {
         statusCode: 200,
         headers: {"Access-Control-Allow-Origin": process.env.CORS_ORIGIN},
         body: JSON.stringify(result)
-    }
+    };
 }
