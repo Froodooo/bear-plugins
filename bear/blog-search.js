520ce9dd687822a75ad22b4536a138ae547286d1
Yordi Verkroost <yordiverkroost@hotmail.com>
2025-06-13 17:03:54 +0200

    Update blog search to support blog posts list

--- bear/blog-search.js
+++ bear/blog-search.js
@@ -1,17 +1,34 @@
-if (document.querySelector(".blog-posts") && document.body.classList.contains("blog")) {
-  document.querySelector("main").insertBefore(
-    Object.assign(
-      document.createElement("input"), {
-      type: "text",
-      id: "searchInput",
-      placeholder: "Search...",
-      style: "display: block;",
-      oninput: (event) => {
-        document.querySelectorAll(".blog-posts li").forEach((post) => {
-          post.style.display = post.textContent.toLowerCase().includes(event.target.value.toLowerCase()) ? "" : "none";
-        })
-      }
-    }),
-    document.querySelector(".blog-posts")
-  );
-}
+  if (document.querySelector(".blog-posts") && document.body.classList.contains("blog")) {
+    document.querySelector("main").insertBefore(
+      Object.assign(
+        document.createElement("input"), {
+        type: "text",
+        id: "searchInput",
+        placeholder: "Search...",
+        style: "display: block;",
+        oninput: (event) => {
+          const monthsToDisplay = new Set();
+
+          document.querySelectorAll(".blog-posts li").forEach((post) => {
+            if (post.textContent.toLowerCase().includes(event.target.value.toLowerCase())) {
+              post.style.display = "";
+
+              const timeElement = post.querySelector('time');
+              const date = new Date(timeElement.getAttribute('datetime'));
+              const monthYear = date.toLocaleString('default', { month: 'long', year: 'numeric' });
+              monthsToDisplay.add(monthYear);
+            } else {
+              post.style.display = "none";
+            }
+          })
+
+          if (event.target.value !== "") {
+            document.querySelectorAll(".blog-posts h2").forEach((h2) => {
+              h2.style.display = monthsToDisplay.has(h2.textContent) ? "" : "none";
+            });
+          }
+        }
+      }),
+      document.querySelector(".blog-posts")
+    );
+  }
